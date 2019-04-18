c64ebb2e84bb7567ff4925f5e789231b79f7b0dd
chikungugna <thomas.rey@posteo.net>
2019-04-19 00:07:08 +0200

    ok

--- src/Controller/ChoiceController.php
+++ src/Controller/ChoiceController.php
@@ -7,49 +7,38 @@ use GuzzleHttp\Client;
 class ChoiceController extends AbstractController
 {
 
-  public function show()
-  {
+    public function show()
+    {
 
 
-  	$client = new Client([
-      // Base URI is used with relative requests
-      'base_uri' => 'http://easteregg.wildcodeschool.fr/api/',
-      // You can set any number of default request options
-    ]);
+        $client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => 'http://easteregg.wildcodeschool.fr/api/',
+        // You can set any number of default request options
+        ]);
 
-		// Initialize Characters array
-  	$characters=[];
-		$i=0;
-		$x=0;
-  	// pick 10 characters and stock into array $data
-  	while (count($characters) < 10) {
-  		
-  		$response = $client->request('GET', 'characters/random');
-  		$temporary_characters=$response->getBody();
-  		$temporary_characters=json_decode($temporary_characters);
-  		if(!in_array($temporary_characters, $characters)) {
-		    $characters[]=$temporary_characters;
-  		}
-  		
-  	}
+        // Initialize Characters array
+      // pick 10 characters and stock into array $data
+        while (count($characters) < 10) {
+            $response = $client->request('GET', 'characters/random');
+            $temporary_characters=$response->getBody();
+            $temporary_characters=json_decode($temporary_characters);
+            if (!in_array($temporary_characters, $characters)) {
+                $characters[]=$temporary_characters;
+            }
+        }
 
-	  if($_SERVER['REQUEST_METHOD']=='POST'){
-		 session_start();
-		if(empty($_SESSION['perso1'])){
-		 $_SESSION['perso1']=array_keys($_POST);
-		}
-		 else{
-		 	$_SESSION['perso2']=array_keys($_POST);
-		 	var_dump($_SESSION);
-
-		 }
-	  }
-	  // session_destroy();
+        if ($_SERVER['REQUEST_METHOD']=='POST') {
+            session_start();
+            if (empty($_SESSION['perso1'])) {
+                $_SESSION['perso1']=array_keys($_POST);
+            } else {
+                $_SESSION['perso2']=array_keys($_POST);
+                var_dump($_SESSION);
+            }
+        }
+      // session_destroy();
     
-    return $this->twig->render('Egg/choicecharacter.html.twig', ['characters'=>$characters]);
-
-  }
-
+        return $this->twig->render('Egg/choicecharacter.html.twig', ['characters'=>$characters]);
+    }
 }
-
-?>
