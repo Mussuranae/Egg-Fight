e5bc93c8cf9fabafdec39e0128da49d98bf037db
Catherine Vincent <catherine.vincent@hotmail.fr>
2019-04-19 00:57:44 +0200

    commentaires en plus

--- src/Controller/FightController.php
+++ src/Controller/FightController.php
@@ -14,6 +14,7 @@ class FightController extends AbstractController
   		'base_uri' => 'http://easteregg.wildcodeschool.fr/api/',
   	]);
 
+  	// Récupération des infos des 2 personnages choisis sur la page choicecharacter.html.twig
   	$response=$client->request('GET', 'characters/'.$_SESSION['perso1'][0]);
   	$characters=$response->getBody();
   	$characters=json_decode($characters);
@@ -22,7 +23,7 @@ class FightController extends AbstractController
   	$characters2=$response2->getBody();
   	$characters2=json_decode($characters2);
 
-  	
+  	// Récupération des infos des 2 oeufs tirés au sort par le click ""
   	$response3=$client->request('GET', 'eggs/random');
   	$egg=$response3->getBody();
   	$egg=json_decode($egg);
@@ -30,7 +31,6 @@ class FightController extends AbstractController
   	$response4=$client->request('GET', 'eggs/random');
   	$egg2=$response4->getBody();
   	$egg2=json_decode($egg2);
-var_dump($egg);
 
     return $this->twig->render('Egg/fight.html.twig', ['characters'=>$characters, 'characters2'=>$characters2, 'egg'=>$egg ,'egg2'=>$egg2]);
   }
