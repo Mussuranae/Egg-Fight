300cbe1be5d2acc4ef793f85a470fddcfde963c3
Catherine Vincent <catherine.vincent@hotmail.fr>
2019-04-18 22:28:35 +0200

    working picker of 10 characters

--- src/Model/CharacterManager.php
+++ src/Model/CharacterManager.php
@@ -1,5 +1,19 @@
 <?php
 
+namespace App\Model;
+
+class CharacterManager extends AbstractManager {
+
+	const (charLife) = 100;
+	const TABLE = 'characters';
+
+	public function__construct() {
+		parent::__construct(self::TABLE);
+	}
+
+
+
+}
 
 
 
