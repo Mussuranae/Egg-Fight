59b4654a174af3700b5cfa3ffade48cc74f2d3b4
chikungugna <thomas.rey@posteo.net>
2019-04-19 00:22:58 +0200

    mis à jour affichage nom des personnages choisis

--- src/Controller/ChoiceController.php
+++ src/Controller/ChoiceController.php
@@ -37,11 +37,18 @@ class ChoiceController extends AbstractController
                 $_SESSION['perso1']=array_keys($_POST);
             } else {
                 $_SESSION['perso2']=array_keys($_POST);
-                var_dump($_SESSION);
+
             }
         }
-      // session_destroy();
+        $response1= $client->request('GET', 'characters/'.$_SESSION['perso1'][0]);
+        $namePerso1=$response1->getBody();
+		$namePerso1=json_decode($namePerso1);
+
+		$response2= $client->request('GET', 'characters/'.$_SESSION['perso2'][0]);
+		$namePerso2=$response2->getBody();
+		$namePerso2=json_decode($namePerso2);
+
     
-        return $this->twig->render('Egg/choicecharacter.html.twig', ['characters'=>$characters]);
+        return $this->twig->render('Egg/choicecharacter.html.twig', ['characters'=>$characters, 'perso1'=>$namePerso1, 'perso2'=>$namePerso2]);
     }
 }
