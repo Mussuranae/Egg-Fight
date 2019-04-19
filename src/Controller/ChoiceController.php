bffcb96047df04ac981a29ec52c917951c7ab224
Catherine Vincent <catherine.vincent@hotmail.fr>
2019-04-19 10:34:07 +0200

    ajout des dégâts des oeufs sur les persos - simple - basique

--- src/Controller/ChoiceController.php
+++ src/Controller/ChoiceController.php
@@ -37,34 +37,28 @@ class ChoiceController extends AbstractController
         // Garde en mémoire dans la session l'info des 2 persos choisis.
         if ($_SERVER['REQUEST_METHOD']=='POST') {
             session_start();
-
             if (empty($_SESSION['perso1'])) {
                 $_SESSION['perso1']=array_keys($_POST);
             } else {
                 $_SESSION['perso2']=array_keys($_POST);
             }
-			//session_destroy();
         }
-      
-		$namePerso1=[];
-		$namePerso2=[];
-        // ICI JE RECUPERE LES INFOS CONCERNANT LES DEUX PERSO. RECHERCHE SUR L'API AVEC L'ID, qui est stocké dans la variable $_SESSION.7
-		if(!empty($_SESSION['perso1'])){
-        $response1= $client->request('GET', 'characters/'.$_SESSION['perso1'][0]);
+
+        // ICI JE RECUPERE LES INFOS CONCERNANT LES DEUX PERSO. RECHERCHE SUR L'API AVEC L'ID, qui est stocké dans la variable $_SESSION.
+        $response1=$client->request('GET', 'characters/'.$_SESSION['perso1'][0]);
         $namePerso1=$response1->getBody();
-		$namePerso1=json_decode($namePerso1);
-		}
-		if(!empty($_SESSION['perso2'])){
-		$response2= $client->request('GET', 'characters/'.$_SESSION['perso2'][0]);
+		$namePerso1=json_decode($namePerso1, true); //avec le ', true' ça renvoit les datas en tableau associatif
+
+		$response2=$client->request('GET', 'characters/'.$_SESSION['perso2'][0]);
 		$namePerso2=$response2->getBody();
-		$namePerso2=json_decode($namePerso2);
-		}
+		$namePerso2=json_decode($namePerso2, true);//avec le ', true' ça renvoit les datas en tableau associatif
+
+
+        $_SESSION['pvPerso1'] = 100;
+        $_SESSION['pvPerso2'] = 100;
+
+
+
         return $this->twig->render('Egg/choicecharacter.html.twig', ['characters'=>$characters, 'perso1'=>$namePerso1, 'perso2'=>$namePerso2]);
     }
-
-    public function deco(){
-    	session_start();
-    	session_destroy();
-    	header('location: /Choice/show');
-	}
 }
