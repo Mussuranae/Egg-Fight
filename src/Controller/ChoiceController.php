dc8e6ca7ed6f28c451dba850e1c76f2ad2eb3834
chikungugna <thomas.rey@posteo.net>
2019-04-19 09:33:53 +0200

    probleme selection perso OK - utiliser bouton recommencer partie avant chaque partie

--- src/Controller/ChoiceController.php
+++ src/Controller/ChoiceController.php
@@ -37,22 +37,36 @@ class ChoiceController extends AbstractController
         // Garde en mémoire dans la session l'info des 2 persos choisis.
         if ($_SERVER['REQUEST_METHOD']=='POST') {
             session_start();
+
             if (empty($_SESSION['perso1'])) {
                 $_SESSION['perso1']=array_keys($_POST);
             } else {
                 $_SESSION['perso2']=array_keys($_POST);
             }
+			//session_destroy();
         }
-
-        // ICI JE RECUPERE LES INFOS CONCERNANT LES DEUX PERSO. RECHERCHE SUR L'API AVEC L'ID, qui est stocké dans la variable $_SESSION.
+        else{
+        	session_destroy();
+		}
+		$namePerso1=[];
+		$namePerso2=[];
+        // ICI JE RECUPERE LES INFOS CONCERNANT LES DEUX PERSO. RECHERCHE SUR L'API AVEC L'ID, qui est stocké dans la variable $_SESSION.7
+		if(!empty($_SESSION['perso1'])){
         $response1= $client->request('GET', 'characters/'.$_SESSION['perso1'][0]);
         $namePerso1=$response1->getBody();
 		$namePerso1=json_decode($namePerso1);
-
+		}
+		if(!empty($_SESSION['perso2'])){
 		$response2= $client->request('GET', 'characters/'.$_SESSION['perso2'][0]);
 		$namePerso2=$response2->getBody();
 		$namePerso2=json_decode($namePerso2);
-
+		}
         return $this->twig->render('Egg/choicecharacter.html.twig', ['characters'=>$characters, 'perso1'=>$namePerso1, 'perso2'=>$namePerso2]);
     }
+
+    public function deco(){
+    	session_start();
+    	session_destroy();
+    	header('location: /Choice/show');
+	}
 }
